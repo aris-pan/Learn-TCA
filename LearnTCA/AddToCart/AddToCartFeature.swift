a26504a8460cde7c85552684700775436175b0a1
aris-pan <121796509+arisreydix@users.noreply.github.com>
2023-03-28 23:13:48 +0300

    remove deprecated domain

--- LearnTCA/AddToCart/AddToCartFeature.swift
+++ LearnTCA/AddToCart/AddToCartFeature.swift
@@ -23,32 +23,3 @@ struct AddToCartFeature: ReducerProtocol {
     }
   }
 }
-
-struct AddToCartDomain {
-  struct State: Equatable {
-    var counter = 0
-  }
-
-  enum Action: Equatable {
-    case didTapPlusButton
-    case didTapMinusButton
-  }
-
-  struct Environment {
-    //
-  }
-
-  static let reducer = AnyReducer<
-    State, Action, Environment
-  > { state, action, environment in
-
-    switch action {
-    case .didTapPlusButton:
-      state.counter += 1
-      return .none
-    case .didTapMinusButton:
-      state.counter -= 1
-      return .none
-    }
-  }
-}
